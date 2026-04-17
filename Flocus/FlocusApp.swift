7961fc1fb526c1b35eed50ed5aa1800121361322
Timofei <timofey.kupriyanov@icloud.com>
2026-04-17 11:52:20 +0300

    initial commit

---
+++ Flocus/FlocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlocusApp.swift
+//  Flocus
+//
+//  Created by Куприянов Тимофей on 17.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct FlocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlocusTests/FlocusTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlocusTests.swift
+//  FlocusTests
+//
+//  Created by Куприянов Тимофей on 17.04.2026.
+//
+
+import Testing
+@testable import Flocus
+
+struct FlocusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
